c539e8ae73a9c2bde6458e3f7b6193c044f442df
Raheemspb <gabibovda@gmail.com>
2023-10-26 16:40:54 +0300

    dev code

--- practic_3/ViewController.swift
+++ practic_3/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        dev code
+//        dev code
+//        dev code
     }
 
 
