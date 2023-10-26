b348911734d1acb37ae76ada38d73e19da110e1f
Raheemspb <gabibovda@gmail.com>
2023-10-26 16:57:09 +0300

    change BG Color Blue

--- practic_3/ViewController.swift
+++ practic_3/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
 
-        view.backgroundColor = .blue
+        view.backgroundColor = .green
     }
 
 
